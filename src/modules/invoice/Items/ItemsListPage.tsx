fd54b71c4d9fb495bd81c1384b647cf80b429b96
ravikantinfineit <ravikant.infineit@gmail.com>
2025-07-08 11:18:05 +0530

    items

--- src/modules/invoice/Items/ItemsListPage.tsx
+++ src/modules/invoice/Items/ItemsListPage.tsx
@@ -19,6 +19,7 @@ const InvoicesItemsListPage: React.FC = () => {
 
   useEffect(() => {
     loadServicesAndInvoices();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.state]);
 
   const loadServicesAndInvoices = async () => {
